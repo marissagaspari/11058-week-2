6abe21a6a99c45af6f9db06b30f9b453da973013
Marissa Gaspari <marissagaspari@hotmail.com>
2019-02-25 16:36:30 +1100

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+
+
+<html>
+<head>
+    <title>Test</title>
+    </head>
+    
+    <head1> Test title </head1>
+    
+<body>
+   <?php echo "hello world" ; ?>
+    
+    
+</body>
+</html>
+    
+    
+    
